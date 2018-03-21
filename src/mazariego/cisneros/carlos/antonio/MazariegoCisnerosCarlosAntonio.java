7b9949f53ddb25c07b8ce9e65affc1e340c5aa8a
carlos o milton <carlos o milton@192.168.1.8>
2018-03-21 20:34:41 -0300

    commit parcial

---
+++ src/mazariego/cisneros/carlos/antonio/MazariegoCisnerosCarlosAntonio.java
@@ -0,0 +1,9 @@
+package mazariego.cisneros.carlos.antonio;
+public class MazariegoCisnerosCarlosAntonio {
+    public static void main(String[] args) {
+        MiEmpresa ventana = new MiEmpresa();
+        ventana.setVisible(true);
+        
+    }
+    
+}
